ebb900d9f77a796c166fc5dcfb66edb365bc2c4d
Florian Rodriguez <florian@negative-network.com>
2024-03-01 16:19:13 +0100

    tags missing (was gitignore)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://tutastroflo.netlify.app'
+  site: 'https://tutastroflo.netlify.app',
+  integrations: [preact()]
 });
